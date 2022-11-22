9177e12cbd5985343d31349b833b7b501df31428
maureenstingel <maureen.stingel@icloud.com>
2022-11-22 08:21:24 +0100

    Erster Stand

---
+++ jss.js
@@ -0,0 +1,10 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log('Ihre Prozentzahl:');
+setTimeout(function( getRandomInt) { (getRandomInt); }, 4000);
+console.log(getRandomInt(100));
+
+
+
